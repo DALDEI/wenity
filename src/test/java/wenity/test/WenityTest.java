a2664ef031f232ed2700791b120f3cfa639e28a4
David Lee <dlee@calldei.com>
2017-01-04 22:01:56 -0500

    Initial Checkin

---
+++ src/test/java/wenity/test/WenityTest.java
@@ -0,0 +1,15 @@
+package wenity.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class WenityTest {
+    @Test
+    public void test1() throws IOException {
+        System.out.println("stub");
+        assert true;
+        
+    }
+    
+}
